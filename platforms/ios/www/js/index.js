b9b9aaa8a5aa69de889cca6bec944d084b48e9ea
Mike Selby <mike.selby@mantlehosting.co.uk>
2019-03-14 15:23:35 +0000

    fix syntax

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -20,6 +20,8 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        
+        
     },
 
     // deviceready Event Handler
@@ -39,7 +41,35 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
+        document.getElementById("barcodetestlink").addEventListener("click", this.testbarcode.bind(this) );
+
         console.log('Received Event: ' + id);
+    },
+
+    testbarcode: function(){
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                alert("We got a barcode\n" +
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled);
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            },
+            {
+                preferFrontCamera : false, // iOS and Android
+                showFlipCameraButton : true, // iOS and Android
+                showTorchButton : true, // iOS and Android
+                torchOn: true, // Android, launch with the torch switched on (if available)
+                saveHistory: false, // Android, save scan history (default false)
+                prompt : "Place the registration barcode inside the scan area", // Android
+                resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                formats : "CODE_128", // default: all but PDF_417 and RSS_EXPANDED
+                disableAnimations : true, // iOS
+                disableSuccessBeep: false // iOS and Android
+            }
+        );
     }
 };
 
